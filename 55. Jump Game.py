56ff3984f758d3314d89159da0d73ce2e397a380
Minji Woo <mjwoo001@gmail.com>
2025-11-23 01:03:21 +0900

    55. Jump Game

---
+++ 55. Jump Game.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/jump-game/
+
+# First Trial
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        answer = False 
+
+        def dfs(cur_index):
+            nonlocal answer
+            if cur_index >= len(nums)-1:
+                answer = True 
+                return 
+            num = nums[cur_index]
+            for jump in range(1, num+1):
+                next_index = cur_index + jump
+                dfs(next_index)
+        dfs(0)
+
+        return answer
+ 
+ # Second Trial : O(N)
+ class Solution:
+    def canJump(self, nums: List[int]) -> bool: 
+        max_index = 0
+        
+        for i, jump in enumerate(nums): 
+            if i > max_index: # this is unreachable  
+                return False 
+            max_index = max(max_index, i + jump)
+
+        if max_index >= len(nums)-1:
+            return True 
+        return False
+        
