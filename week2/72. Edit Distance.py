17fdaf12abc9abc9e608eefff3ee0b37f38a2cd1
Minji Woo <mjwoo001@gmail.com>
2025-12-03 06:59:30 +0900

    153. LeetCode

--- week2/72. Edit Distance.py
+++ week2/72. Edit Distance.py
@@ -36,12 +36,44 @@ class Solution:
 
         for i in range(1, n+1):
             for j in range(1, m+1):
-                if word1[i-1] != word2[j-1]:
+                if word1[i-1] != word2[j-1]: # delete / update cases (operation + 1)
                     dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
-                else: 
+                else: # no need to operate
                     dp[i][j] = dp[i-1][j-1]
         
         for i in dp:
             print(*i)
+
+        return dp[n][m] 
+"""
+if one word is shorter than the other words, it's not accurate
+
+0 0 0 0
+0 1 1 1
+0 1 1 2
+0 0 1 2
+0 1 1 1
+0 1 2 2
+"""       
+
+# Try 3 
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+        # need to initialize the dp array 
+        
+        for i in range(1, n+1):
+            dp[i][0] = i
+        for j in range(1, m+1):
+            dp[0][j] = j
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] != word2[j-1]:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+                else: 
+                    dp[i][j] = dp[i-1][j-1]
 
         return dp[n][m] 
