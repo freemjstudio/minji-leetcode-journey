d1b172ff995075c8e2d9a363564f96350d5521a2
freemjstudio <mjwoo001@gmail.com>
2025-12-07 13:44:11 +0900

    14. Longest Common Prefix

--- week3/14. Longest Common Prefix.py
+++ week3/14. Longest Common Prefix.py
@@ -14,6 +14,9 @@
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+
         answer = ""
         is_prefix = True
         min_length = 200
@@ -21,13 +24,30 @@ class Solution:
             min_length = min(min_length, len(strs[i]))
 
         for i in range(min_length):
-            char = strs[i][i]
             for j in range(len(strs) -1):
                 if strs[j][i] != strs[j+1][i]:
                     is_prefix = False
                     break 
+                else: 
+                    char = strs[j][i] 
             if is_prefix:
                 answer += char
             else:
                 break 
+        return answer 
+    
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str: # ["flower","flow","flight"]
+        answer = ""
+        strs = sorted(strs)
+        print(strs) # ['flight', 'flow', 'flower'] sorted in lexicographical order
+        first = strs[0]
+        last = strs[-1]
+
+        for i in range(min(len(first), len(last))):
+            if (first[i] != last[i]):
+                return answer
+            answer += first[i]
         return answer 
